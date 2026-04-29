92bc1cc13f0679442e8cf171327ef37a4946cbce
Sahil <sahil@example.com>
2026-04-29 11:23:39 +0530

    Fix scroll reveal threshold so all sections are visible

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const revealObserver = new IntersectionObserver((entries) => {
       revealObserver.unobserve(entry.target);
     }
   });
-}, { threshold: 0.1 });
+}, { threshold: 0.05, rootMargin: '0px 0px -50px 0px' });
 revealEls.forEach(el => revealObserver.observe(el));
 
 // ===== READING PROGRESS BAR =====
